927b00c68707555901093b68dfd972864021fa3a
HongxiChen <hc413@duke.edu>
2025-07-23 21:18:12 -0400

    test14

--- frontend/dist/js/RegisterPage-8oGvXTZ4.js
+++
@@ -1,7 +0,0 @@
-import { j as jsxRuntimeExports } from "./index-D-bA2lkg.js";
-const RegisterPage = () => {
-  return /* @__PURE__ */ jsxRuntimeExports.jsx("div", { className: "p-8", children: /* @__PURE__ */ jsxRuntimeExports.jsx("h1", { className: "text-2xl font-bold", children: "Register" }) });
-};
-export {
-  RegisterPage
-};
